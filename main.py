8d41b21a2fef63fb483f04761613ac46e40a3f45
Jiwoo Shin <87713422+jiwoo-isyse@users.noreply.github.com>
2022-05-29 16:53:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from graph_function import get_graph, save_graph_png, apply_node2vec, save_ranki
 parser = argparse.ArgumentParser(description='CS492 Project')
 parser.add_argument('--start', default='2022-04-25', type=str)
 parser.add_argument('--end', default='2022-05-23', type=str)
+parser.add_argument('--p', default=1, type=float)
+parser.add_argument('--q', default=2, type=float)
 parser.add_argument('--cor_type', default='both', type=str, help='positive, negative, both')
-parser.add_argument('--threshold', default=0.9, type=float)
+parser.add_argument('--threshold', default=0.8, type=float)
 parser.add_argument('--market_index', default='SnP500', type=str, help='Nasdaq or SnP500')
 parser.add_argument('--company_list', default='SnP500', type=str, help='SnP500 or DOW')
 parser.add_argument('--save_folder', default='results', type=str)
@@ -38,7 +40,7 @@ def main():
     save_graph_png(G, save_path)
 
     # get node vectors
-    node_vecs = apply_node2vec(G)
+    node_vecs = apply_node2vec(G, args.p, args.q)
     
     # save ranking matrix as csv
     save_ranking(node_vecs, save_path)
